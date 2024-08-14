e89407c0fa728055b08706db923331087ec215e2
VENXIA <vniu740>
2024-08-14 23:01:41 +1200

    style: Css for elements on the page

--- src/app/calculator/page.js
+++ src/app/calculator/page.js
@@ -3,15 +3,16 @@ import "./calculator.css";
 export default function CalculatorPage() {
   return (
     <main>
-      <div className="header">
-        <h1>Grade Calculator</h1>
-        <div className="createCourse">
-          <input placeholder="Enter Course Name"></input>
-          <button>+</button>
+      <div className="gradeCalculator">
+        <div className="header">
+          <h1>Grade Calculator</h1>
+          <div className="createCourse">
+            <input placeholder="Enter Course Name"></input>
+            <button>+</button>
+          </div>
         </div>
-      </div>
-      <div className="overallGrade">Current GPA:/9</div>
-      <div className="courses">
+        <div className="overallGrade">Current GPA:/9</div>
+        <div className="courses"></div>
       </div>
     </main>
   );
