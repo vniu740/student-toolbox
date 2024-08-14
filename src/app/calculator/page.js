994580e39baa484fe607668da0dd28a192367b1e
VENXIA <vniu740>
2024-08-14 22:52:13 +1200

    feat: Basic structure of JSX elements for header

--- src/app/calculator/page.js
+++ src/app/calculator/page.js
@@ -1,9 +1,18 @@
-import Title from "@/components/title"
+import "./calculator.css";
 
 export default function CalculatorPage() {
   return (
     <main>
-      <Title text="Calculator" />
+      <div className="header">
+        <h1>Grade Calculator</h1>
+        <div className="createCourse">
+          <input placeholder="Enter Course Name"></input>
+          <button>+</button>
+        </div>
+      </div>
+      <div className="overallGrade">Current GPA:{gpa}/9</div>
+      <div className="courses">
+      </div>
     </main>
-  )
+  );
 }
