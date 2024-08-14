756fb514df2c414cc0efd0715df9a69b1d7b2bb8
VENXIA <vniu740>
2024-08-15 00:19:23 +1200

    feat: Course dropdown showing what will be the assignments

--- src/components/calculator/course.jsx
+++ src/components/calculator/course.jsx
@@ -1,9 +1,17 @@
 import "./css/course.css";
+import { useState } from "react";
 
-export default function course(props) {
+
+export default function Course(props) {
+  const [showAssignments, setShowAssignments] = useState(false);
+
+  const handleCourseClick = () => {
+    setShowAssignments(!showAssignments);
+  };
+
   return (
     <div className="course">
-      <div className="courseInfo">
+      <div className="courseInfo" onClick={handleCourseClick}>
         <div>
           <div className="courseName">Course Name: {props.courseName} </div>
           <div className="letterGrade">X</div>
@@ -19,6 +27,12 @@ export default function course(props) {
           </button>
         </div>
       </div>
+      {showAssignments && (
+        <div className="courseAssignments">
+          <h1>Will be assignment component</h1>
+          <button>New Assignment +</button>
+        </div>
+      )}
     </div>
   );
 }
