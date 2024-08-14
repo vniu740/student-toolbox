dd68c5daabafac8a750859fcfcae4fcd315541ca
VENXIA <vniu740>
2024-08-15 00:04:52 +1200

    style: Css for course course component

--- src/components/calculator/course.jsx
+++ src/components/calculator/course.jsx
@@ -1,14 +1,12 @@
-
+import "./css/course.css";
 
 export default function course(props) {
- 
-
   return (
     <div className="course">
       <div className="courseInfo">
         <div>
           <div className="courseName">Course Name: {props.courseName} </div>
-          <div className="letterGrade"></div>
+          <div className="letterGrade">X</div>
         </div>
         <div className="gradeSummary">
           <div className="totalGrade">Total %: %</div>
@@ -23,4 +21,4 @@ export default function course(props) {
       </div>
     </div>
   );
-};
+}
